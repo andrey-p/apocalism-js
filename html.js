8895e86436163c69e067ff6301cc39fcd4eb522a
Andrey Pissantchev <pissantchev@gmail.com>
2013-09-22 22:25:00 +0100

    added basic styling for html template

--- html.js
+++ html.js
@@ -1,15 +1,43 @@
 /*jslint indent: 2, node: true*/
 "use strict";
 
-var pandoc = require("pdc");
+var pandoc = require("pdc"),
+  fs = require("fs"),
+  sass = require("node-sass");
 
 exports.generateFromMarkdown = function (markdown, callback) {
-  pandoc(markdown, "markdown", "html", function (err, result) {
+  var htmlMarkup;
+
+  function convertedSassToCss(err, result) {
+    var styleTag;
     if (err) {
       callback(err);
       return;
     }
 
+    styleTag = "<style type='text/css'>" + result + "</style>";
+
+    callback(null, styleTag + htmlMarkup);
+  }
+
+  function convertedMarkdownToHtml(err, result) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    htmlMarkup = result;
+
+    sass.render({
+      file: "./template/style.scss",
+      success: function (css) {
+        convertedSassToCss(null, css);
+      },
+      error: function (err) {
+        convertedSassToCss(err);
+      }
+    });
     callback(null, result);
-  });
+  }
+  pandoc(markdown, "markdown", "html", convertedMarkdownToHtml);
 };
