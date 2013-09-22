f6403ef66102b0d0c8b0e4da04b13ee88ee81bde
Andrey Pissantchev <pissantchev@gmail.com>
2013-09-22 19:12:15 +0100

    added basic cli

--- main.js
+++ main.js
@@ -2,5 +2,22 @@
 /*jslint indent: 2, node: true*/
 "use strict";
 
-exports.convert = function () {
+var program = require("commander");
+
+function compilePdf(file) {
+  console.log("compiling pdf...");
 };
+
+program
+  .version("0.0.1")
+  .usage("[command] <file>");
+
+program
+  .command("pdf <file>")
+  .action(compilePdf);
+
+program
+  .command("*")
+  .action(compilePdf);
+  
+program.parse(process.argv);
