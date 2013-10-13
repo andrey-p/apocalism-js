8b888f7ff3214e96d878bc0c31a151374f3638eb
Andrey Pissantchev <pissantchev@gmail.com>
2013-10-13 12:16:10 +0100

    wired up pdf and template, created multuple page generation

--- pdf.js
+++ pdf.js
@@ -2,13 +2,14 @@
 "use strict";
 
 var phantomWrapper = require("./phantom-wrapper.js"),
-  stock = require("./dimensions.js").stock;
+  os = require("os"),
+  template = require("./template.js");
 
 exports.generateFromHtml = function (markup, path, callback) {
   var page;
 
   function pageRendered(err) {
-    callback(err);
+    callback(err, path);
   }
 
   function setContent(err) {
@@ -37,11 +38,46 @@ exports.generateFromHtml = function (markup, path, callback) {
     page = phantomPage;
     page.set("paperSize", {
       // do I still need bleed * 2 if one of the sides is not being cut off?
-      width: (stock.width + stock.bleed * 2) + "mm",
-      height: (stock.height + stock.bleed * 2) + "mm",
+      width: (template.stock.width + template.stock.bleed * 2) + "mm",
+      height: (template.stock.height + template.stock.bleed * 2) + "mm",
       border: "0mm" // border is handled by html.js
     }, setPaperSize);
   }
 
   phantomWrapper.getPage(gotPage);
+};
+
+exports.generatePagesFromHtml = function (pages, callback) {
+  var page,
+    pathsToPdfs = [],
+    path;
+
+  if (pages.length === 0) {
+    callback(null, []);
+    return;
+  }
+
+  page = pages.shift();
+
+  function generatedPdf(err, pathToPdf) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    pathsToPdfs.push(pathToPdf);
+
+    if (pages.length === 0) {
+      callback(null, pathsToPdfs);
+      return;
+    }
+
+    page = pages.shift();
+    path = os.tmpDir() + "/apoc_js_" + Date.now() + ".pdf";
+    exports.generateFromHtml(page, path, generatedPdf);
+  }
+
+  path = os.tmpDir() + "/apoc_js_" + Date.now() + ".pdf";
+
+  exports.generateFromHtml(page, path, generatedPdf);
 };
