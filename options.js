94328b5eb4bd3091513a170ac92b0c0ea0e8dd4a
Andrey Pissantchev <pissantchev@gmail.com>
2013-10-25 17:35:56 +0100

    Added options
    you pass a hash of options to it, and it parses, populates them etc
    
    it also initialises the correct template

---
+++ options.js
@@ -0,0 +1,75 @@
+/*jslint indent: 2, node: true*/
+"use strict";
+
+var defaults = {
+  output: "output/output.pdf",
+  pathToImages: "images/",
+  stock: "148x210mm",
+  bleed: "2mm",
+  margin: "15mm 15mm 20mm 15mm",
+  template: "default",
+  title: null,
+  author: null
+},
+  template = require("./template.js"),
+  changeCase = require("case");
+
+function parseStock(stock) {
+  var regex = /(\d+)x(\d+)/,
+    match = stock.match(regex);
+
+  return {
+    width: parseInt(match[1], 10),
+    height: parseInt(match[2], 10)
+  };
+}
+
+function parseBleed(bleed) {
+  var regex = /\d+/,
+    match = bleed.match(regex);
+
+  return parseInt(match[0], 10);
+}
+
+function parseMargins(margins) {
+  var regex = /(\d+)/g,
+    match = margins.match(regex);
+
+  return {
+    top: parseInt(match[0], 10),
+    outer: parseInt(match[1], 10),
+    bottom: parseInt(match[2], 10),
+    spine: parseInt(match[3], 10),
+  };
+}
+
+exports.set = function (optionsToSet, callback) {
+  var prop,
+    normalisedOptions = {};
+
+  // normalise option keys
+  for (prop in optionsToSet) {
+    if (optionsToSet.hasOwnProperty(prop)) {
+      normalisedOptions[changeCase.camel(prop)] = optionsToSet[prop];
+    }
+  }
+
+  for (prop in defaults) {
+    if (defaults.hasOwnProperty(prop)) {
+      if (normalisedOptions.hasOwnProperty(prop)) {
+        exports[prop] = normalisedOptions[prop];
+      } else if (defaults[prop] !== null) {
+        exports[prop] = defaults[prop];
+      } else {
+        callback("missing required option: " + prop);
+        return;
+      }
+    }
+  }
+
+  exports.stock = parseStock(exports.stock);
+  exports.bleed = parseBleed(exports.bleed);
+  exports.margin = parseMargins(exports.margin);
+
+  template.init(exports.template, callback);
+};
---
+++ test/options.js
@@ -0,0 +1,100 @@
+/*jslint indent: 2, node: true*/
+/*globals it, describe, beforeEach, after, before*/
+"use strict";
+
+var should = require("should"),
+  template = require("../template.js"),
+  options = require("../options.js");
+
+describe("options", function () {
+  describe("#set()", function () {
+    var validInput;
+    beforeEach(function () {
+      validInput = {
+        output: "test/path.pdf",
+        pathToImages: "test/path/",
+        template: "default",
+        title: "test expectations",
+        author: "testy mctest"
+      };
+    });
+    it("should set all the options passed to it", function (done) {
+      options.set(validInput, function (err) {
+        var prop;
+        should.not.exist(err);
+        for (prop in validInput) {
+          if (validInput.hasOwnProperty(prop)) {
+            options[prop].should.equal(validInput[prop]);
+          }
+        }
+        done();
+      });
+    });
+    it("should should complain if options without a default aren't set", function (done) {
+      delete validInput.title;
+      options.set(validInput, function (err) {
+        should.exist(err);
+        done();
+      });
+    });
+    it("should not set options that don't exist", function (done) {
+      validInput.thisShouldNotBeSet = "this";
+      options.set(validInput, function (err) {
+        options.should.not.have.property("thisShouldNotBeSet");
+        done();
+      });
+    });
+    it("should set options even when keys aren't camel cased", function (done) {
+      delete validInput.title;
+      delete validInput.pathToImages;
+      validInput.Title = "title";
+      validInput["Path to images"] = "path/to/images";
+      options.set(validInput, function (err) {
+        should.not.exist(err);
+        options.title.should.equal(validInput.Title);
+        options.pathToImages.should.equal(validInput["Path to images"]);
+        done();
+      });
+    });
+    it("should parse the dimensions passed to stock correctly", function (done) {
+      validInput.stock = "100x105mm";
+      options.set(validInput, function (err) {
+        should.not.exist(err);
+        options.stock.width.should.equal(100);
+        options.stock.height.should.equal(105);
+        done();
+      });
+    });
+    it("should parse the dimensions passed to bleed correctly", function (done) {
+      validInput.bleed = "5mm";
+      options.set(validInput, function (err) {
+        should.not.exist(err);
+        options.bleed.should.equal(5);
+        done();
+      });
+    });
+    it("should parse the dimensions passed to margin", function (done) {
+      validInput.margin = "1 2mm 3mm 4mm";
+      options.set(validInput, function (err) {
+        should.not.exist(err);
+        options.margin.top.should.equal(1);
+        options.margin.outer.should.equal(2);
+        options.margin.bottom.should.equal(3);
+        options.margin.spine.should.equal(4);
+        done();
+      });
+    });
+    it("should initialise the template", function (done) {
+      var tempInit = template.init;
+      template.init = function (templateName) {
+        templateName.should.equal(validInput.template);
+        template.init = tempInit;
+        done();
+      };
+
+      options.set(validInput, function (err) {
+        throw new Error("should not get here");
+      });
+    });
+  });
+});
