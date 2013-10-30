718023b237613559e48257b078fd11ecc4bd9392
Andrey Pissantchev <pissantchev@gmail.com>
2013-10-30 18:51:59 +0000

    Moved createPage function into a separate phantom script
    this makes things ridiculously easy when profiling / improving the
    create page script

--- phantom-wrapper.js
+++ phantom-wrapper.js
@@ -2,7 +2,72 @@
 "use strict";
 
 var phantom = require("node-phantom"),
+  fs = require("fs"),
+  exec = require("child_process").exec,
+  os = require("os"),
   phantomInstance;
+
+exports.createPage = function (blankPage, markup, dimensions, callback) {
+  var pathToBlankPage = os.tmpdir() + "/tmp_blank_page.html",
+    pathToOverflow = os.tmpdir() + "/tmp_content.html",
+    updatedPage,
+    updatedOverflow;
+
+  function readPage(err, data) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    updatedPage = data;
+
+    callback(null, updatedPage, updatedOverflow);
+  }
+
+  function readOverflow(err, data) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    updatedOverflow = data;
+
+    fs.readFile(pathToBlankPage, { encoding: "utf8" }, readPage);
+  }
+
+  function doneWithScript(err, stdout, stderr) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    fs.readFile(pathToOverflow, { encoding: "utf8" }, readOverflow);
+  }
+
+  function wroteBlankPage(err) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    exec("phantomjs " + __dirname + "/phantom-scripts/create-page.js "
+        + pathToBlankPage + " "
+        + pathToOverflow + " "
+        + dimensions.width + " "
+        + dimensions.height, doneWithScript);
+  }
+
+  function wroteOveflow(err) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    fs.writeFile(pathToBlankPage, blankPage, wroteBlankPage);
+  }
+
+  fs.writeFile(pathToOverflow, markup, wroteOveflow);
+};
 
 exports.getInstance = function (callback) {
   if (phantomInstance) {
@@ -34,7 +99,13 @@ exports.getPage = function (callback) {
       return;
     }
 
-    instance.createPage(callback);
+    instance.createPage(function (err, page) {
+      page.onConsoleMessage = function (msg) {
+        console.log("phantom says: " + msg);
+      };
+
+      callback(err, page);
+    });
   }
 
   exports.getInstance(gotPhantomInstance);
