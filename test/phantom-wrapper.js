0cdd119c1962cf55c26fa24847d6e9ef73e6fdbe
Andrey Pissantchev <pissantchev@gmail.com>
2013-12-18 17:33:56 +0000

    Switched to args object for phantom-wrapper#generatePdfPage method
    using this really cool package called arg-err

--- test/phantom-wrapper.js
+++ test/phantom-wrapper.js
@@ -11,25 +11,27 @@ var should = require("should"),
   htmlMarkup;
 
 describe("phantom-wrapper", function () {
-  var pathToPdf,
-    dimensions;
+  var args;
 
   before(function () {
     // need this because the phantom script creates its own dir
     opts.pathToOutput = os.tmpdir() + "/apoc_out/";
-    dimensions = {
-      width: opts.stock.width + opts.bleed * 2,
-      height: opts.stock.height + opts.bleed * 2
+
+    args = {
+      pathToPdf: opts.pathToOutput + "output.pdf",
+      markup: "<p>Hello!</p>",
+      dimensions: {
+        width: opts.stock.width + opts.bleed * 2,
+        height: opts.stock.height + opts.bleed * 2
+      }
     };
-    pathToPdf = opts.pathToOutput + "output.pdf";
-    htmlMarkup = "<p>Hello!</p>";
   });
   after(function (done) {
     fs.remove(opts.pathToOutput, done);
   });
   describe("#generatePdfPage()", function () {
     it("should generate a pdf from an html string", function (done) {
-      phantomWrapper.generatePdfPage(htmlMarkup, pathToPdf, dimensions, function (err) {
+      phantomWrapper.generatePdfPage(args, function (err) {
         should.not.exist(err);
         // check if file is a pdf - this has the side effect of checking if file exists, too
         helper.getFileMimeType(opts.pathToOutput + "output.pdf", function (err, mimetype) {
@@ -40,9 +42,9 @@ describe("phantom-wrapper", function () {
       });
     });
     it("should output pages in A5 format + bleed if the default template is being used", function (done) {
-      phantomWrapper.generatePdfPage(htmlMarkup, pathToPdf, dimensions, function (err) {
+      phantomWrapper.generatePdfPage(args, function (err) {
         should.not.exist(err);
-        helper.getPdfPaperSize(pathToPdf, function (err, paperSize) {
+        helper.getPdfPaperSize(args.pathToPdf, function (err, paperSize) {
           should.not.exist(err);
           // 431 x 607 pts is 152 x 214.1mm, which is more or less A5 with 2mm bleed
           paperSize.should.include("431 x 607");
