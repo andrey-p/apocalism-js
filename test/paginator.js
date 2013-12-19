1e72b10f42fb91a9e7037e143300da0181911d3f
Andrey Pissantchev <pissantchev@gmail.com>
2013-12-19 16:42:01 +0000

    paginator improvements
    - un-exposed createPage method
    - upped coverage and test quality

--- test/paginator.js
+++ test/paginator.js
@@ -1,77 +1,159 @@
 /*jslint indent: 2, node: true*/
-/*globals it, describe, after, beforeEach, before*/
+/*globals it, describe, after, beforeEach, afterEach, before*/
 "use strict";
 
 var should = require("should"),
   options = require("../lib/options.js"),
   template = require("../lib/template.js"),
   paginator = require("../lib/paginator.js"),
+  monkey = require("monkey-patch"),
+  phantomWrapper = require("../lib/phantom-wrapper.js"),
   progress = require("../lib/progress.js"),
   lipsum = require("lorem-ipsum"),
   helper = require("./helper.js"),
-  opts = helper.getDefaultOpts(),
-  emptyPageMarkup;
+  opts = helper.getDefaultOpts();
 
 describe("paginator", function () {
   before(function (done) {
     progress.init(opts, function () {
-      template.init(opts, function () {
-        paginator.init(opts, function () {
-          emptyPageMarkup = template.getBlankPage("page");
-          done();
-        });
+      paginator.init(opts, function () {
+        done();
       });
     });
   });
   describe("#paginate()", function () {
-    var lipsumOptions,
-      markup,
-      args;
+    var args;
 
     beforeEach(function () {
-      lipsumOptions = {
-        format: "html",
-        units: "paragraph"
+      args = {
+        templateName: "pdf",
+        sectionName: "body",
+        content: "<p>heyo!</p>"
       };
-      args = {};
+      monkey.patch(template, {
+        getBlankPage: function () {
+          return "<html></html>";
+        }
+      });
+      monkey.patch(phantomWrapper, {
+        createPage: function (args, callback) {
+          should.exist(args);
+          callback(null, "<html><p>heyo!</p></html>", "");
+        }
+      });
     });
-    it("should output a single page if the content is small enough to fit", function (done) {
-      lipsumOptions.count = 1;
-      lipsumOptions.units = "sentence";
-      markup = "<p>" + lipsum(lipsumOptions) + "<p>";
-      args = { templateName: "pdf", sectionName: "body", content: markup };
+    afterEach(function () {
+      monkey.unpatch(template);
+      monkey.unpatch(phantomWrapper);
+    });
+    it("should request a blank page from the template module with all the right classes", function (done) {
+      var called = false;
+      template.getBlankPage = function (templateName, data) {
+        templateName.should.equal("page");
+        (typeof data).should.equal("object");
+        data.pageNumber.should.equal(1);
+        (data.classes instanceof Array).should.equal(true);
+        data.classes.should.include(args.sectionName);
+        data.classes.should.include("verso");
+        data.classes.should.include("page-1");
 
-      paginator.paginate(args, function (err, pages) {
+        called = true;
+        return "<html></html>";
+      };
+      paginator.paginate(args, function (err) {
+        called.should.equal(true);
         should.not.exist(err);
-        pages.should.have.length(1);
         done();
       });
     });
-    it("should output multiple pages if the content is larger", function (done) {
-      lipsumOptions.count = 10;
-      markup = lipsum(lipsumOptions);
-      args = { templateName: "pdf", sectionName: "body", content: markup };
-
-      paginator.paginate(args, function (err, pages) {
+    it("should call the createPage method on the phantomWrapper with all the right args", function (done) {
+      var called = false;
+      phantomWrapper.createPage = function (args, callback) {
+        should.exist(args);
+        args.blankPage.should.equal("<html></html>");
+        args.markup.should.equal("<p>heyo!</p>");
+        called = true;
+        callback(null, "<html><p>heyo!</p></html>", "");
+      };
+      paginator.paginate(args, function (err) {
+        called.should.equal(true);
         should.not.exist(err);
-        pages.length.should.be.above(1);
         done();
       });
     });
-    it("should output pages with full head and body tags", function (done) {
-      lipsumOptions.count = 10;
-      markup = lipsum(lipsumOptions);
-      args = { templateName: "pdf", sectionName: "body", content: markup };
+    it("should call the createPage method twice if it returns a leftover the first time round", function (done) {
+      var times = 0;
+      phantomWrapper.createPage = function (args, callback) {
+        var leftover = "";
+        should.exist(args);
+        times += 1;
 
-      paginator.paginate(args, function (err, pages) {
+        if (times === 1) {
+          leftover = "<p>I'm a leftover!</p>";
+        }
+
+        callback(null, "<html><p>heyo!</p></html>", leftover);
+      };
+      paginator.paginate(args, function (err) {
+        times.should.equal(2);
         should.not.exist(err);
-        pages.forEach(function (page) {
-          page.should.include("<html>");
-          page.should.include("<head>");
-          page.should.include("</head>");
-          page.should.include("<body");
-          page.should.include("</body>");
-        });
+        done();
+      });
+    });
+  });
+  describe("#createStandalonePage()", function () {
+    var args;
+
+    beforeEach(function () {
+      args = {
+        sectionName: "body",
+        content: "<p>heyo!</p>"
+      };
+      monkey.patch(template, {
+        getBlankPage: function () {
+          return "<html></html>";
+        }
+      });
+      monkey.patch(phantomWrapper, {
+        createPage: function (args, callback) {
+          should.exist(args);
+          callback(null, "<html><p>heyo!</p></html>", "");
+        }
+      });
+    });
+    afterEach(function () {
+      monkey.unpatch(template);
+      monkey.unpatch(phantomWrapper);
+    });
+    it("should request a blank page from the template module with all the right classes", function (done) {
+      var called = false;
+      template.getBlankPage = function (templateName, data) {
+        templateName.should.equal("page");
+        (typeof data).should.equal("object");
+        (data.classes instanceof Array).should.equal(true);
+        data.classes.should.include(args.sectionName);
+
+        called = true;
+        return "<html></html>";
+      };
+      paginator.createStandalonePage(args, function (err) {
+        called.should.equal(true);
+        should.not.exist(err);
+        done();
+      });
+    });
+    it("should call the createPage method on the phantomWrapper with all the right args", function (done) {
+      var called = false;
+      phantomWrapper.createPage = function (args, callback) {
+        should.exist(args);
+        args.blankPage.should.equal("<html></html>");
+        args.markup.should.equal("<p>heyo!</p>");
+        called = true;
+        callback(null, "<html><p>heyo!</p></html>", "");
+      };
+      paginator.createStandalonePage(args, function (err) {
+        called.should.equal(true);
+        should.not.exist(err);
         done();
       });
     });
