195a181c690e6bae9f2d562d8ab044c486d0be89
Andrey Pissantchev <pissantchev@gmail.com>
2014-11-03 17:24:32 +0000

    Added --no-images flag, fixes #25
    In case there's a story that doesn't use any image-related features at
    all, this flag can prevent apocalism.js from erroring out

--- bin/apocalism-cli.js
+++ bin/apocalism-cli.js
@@ -16,6 +16,7 @@ program
   .option("--no-concat", "output a file per page as opposed to a single file")
   .option("--lo-res", "use low resolution images")
   .option("-o, --output [format]", "output format, possible values pdf (default) and html", "pdf")
+  .option("--no-images", "assume this project uses no images (speeds things up and prevents things breaking if an images folder isn't found")
   .option("--webpage", "shorthand for -o html --lo-res --no-bleed --ignore-blank")
   .option("--download", "shorthand for -o pdf --lo-res --no-bleed --ignore-blank")
   .option("--debug", "shorthand for -o html --no-concat (useful for debugging layout issues)")
@@ -50,6 +51,7 @@ main.compile(program.args[0], {
   quiet: program.quiet,
   hasBleed: program.bleed,
   concat: program.concat,
+  images: program.images,
   loRes: program.loRes,
   ignoreBlank: program.ignoreBlank
 });
